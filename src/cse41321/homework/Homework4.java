e66a66f9866b9cdd8f66c77fc0c62f602c8d3709
Yahya ElGawady <yahya.elgawady@gmail.com>
2021-07-20 22:57:47 -0500

    Create Homework4.java

---
+++ src/cse41321/homework/Homework4.java
@@ -0,0 +1,59 @@
+package cse41321.homework;
+
+import cse41321.containers.Stack;
+
+public class Homework4 {
+	/**
+	 * Adds two large numbers
+	 * @param num1 first Number to be added 
+	 * @param num2 second Number to be added 
+	 */
+	public static void addLargeNumbers(String num1, String num2) {
+		// holds the digits of the first number
+		Stack<Integer> firstNum = new Stack<Integer>();
+		//iterates through the char array version of the first string
+		for(char numeral : num1.toCharArray()) {
+			//puts the numeric values of numeral on the stack
+			firstNum.push(Character.getNumericValue(numeral));
+		}
+		// holds the digits of the second number
+		Stack<Integer> secondNum = new Stack<Integer>();
+		//iterates through the char array version of the second string
+		for(char numeral : num2.toCharArray()) {
+			//puts the numeric values of numeral on the stack
+			secondNum.push(Character.getNumericValue(numeral));
+		}
+		//temporary holds the digits addition
+		int result = 0;
+		//holds the digits of the result
+		Stack<Integer> Result = new Stack<Integer>();
+		//repeats while either number isn't zero and result var isnt zero
+		while (!firstNum.isEmpty() || !secondNum.isEmpty() || result != 0) {
+			// if the the number isn't empty then add the last number added to the stack
+			if(!firstNum.isEmpty()) {
+				result += firstNum.pop();
+			}
+			// if the the number isn't empty then add the last number added to the stack
+			if(!secondNum.isEmpty()) {
+				result += secondNum.pop();
+			}
+			// push the units digit of result onto the result stack
+			Result.push(result % 10);
+			// keep the tens digit
+			result /= 10;
+		} 
+		while (!Result.isEmpty()) {
+			System.out.print(Result.pop());
+		}
+		System.out.println();
+	}
+	
+	public static void main (String[] args) {
+		//Example One
+		addLargeNumbers("98732383294829432843298578","98732383294829432843298578");
+		//Example Two
+		addLargeNumbers("98732383294829432843298578","1");
+		//Example Three
+		addLargeNumbers("9873238329482946876868768768768768768768689403092832843298578","73285798372985729579832759879898732383294829432843298578");
+	}
+}
